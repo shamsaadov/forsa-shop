ba4e5ee4c22c04b260451e9b37fc0549caa129ec
shamsaadov <shamsadovkh@gmail.com>
2025-06-07 17:06:30 +0300

    Попытка изменить тип цены

--- server/src/config/migrations/20240321_add_price_type.sql
+++
@@ -1,6 +0,0 @@
--- Добавление поля price_type в таблицу products
-ALTER TABLE products
-ADD COLUMN price_type ENUM('square_meter', 'linear_meter', 'piece') NOT NULL DEFAULT 'square_meter' AFTER price;
-
--- Обновление существующих записей
-UPDATE products SET price_type = 'square_meter' WHERE price_type IS NULL; 
